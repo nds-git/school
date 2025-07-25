9d0ec48c26cb19cb324702d40e99d88681ca7269
nosov <noc.dima.inet@gmail.com>
2025-07-25 08:49:41 +0300

    add rabbit sum lecture for course

--- src/Controller/Amqp/UserSumLecturePoint/UserSumLecturePointConsumer.php
+++ src/Controller/Amqp/UserSumLecturePoint/UserSumLecturePointConsumer.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Amqp\UserSumLecturePoint;
 
 use App\Controller\Amqp\UserSumLecturePoint\Input\UserSumLectureMessage;
-use App\Domain\Model\LectureUserPointModel;
 use App\Domain\Service\ExerciseUserPointService;
 use App\Domain\Service\LectureUserPointService;
+use App\Domain\Service\CourseUserPointService;
 use App\Application\RabbitMq\AbstractConsumer;
+use App\Domain\Model\LectureUserPointModel;
+use App\Domain\Model\CourseUserPointModel;
 use App\Domain\Service\LectureService;
 use App\Domain\Service\ModelFactory;
 use App\Domain\Service\UserService;
@@ -16,12 +18,14 @@ use App\Domain\Entity\User;
 class UserSumLecturePointConsumer extends AbstractConsumer
 {
     public function __construct(
+        /** @var ModelFactory<CourseUserPointModel> */
         /** @var ModelFactory<LectureUserPointModel> */
         private readonly ModelFactory $modelFactory,
         private readonly UserService $userService,
         private readonly LectureService $lectureService,
         private readonly ExerciseUserPointService $exerciseUserPointService,
         private readonly LectureUserPointService $lectureUserPointService,
+        private readonly CourseUserPointService $courseUserPointService,
     ) {
     }
 
@@ -65,6 +69,29 @@ class UserSumLecturePointConsumer extends AbstractConsumer
             );
 
             $this->lectureUserPointService->createLectureUserPoint($lectureUserPointModel);
+
+            $course = $lecture->getCourse();
+            // из одной лекции извлекаем все активные ДЗ
+            foreach ($course->getLectures() as $lecture) {
+                if ($lecture['isActive'] === 0) {
+                    $lectureIds[] = $lecture['id'];
+                }
+            }
+
+            // теперь суммируем все баллы нашего юзера по конкретному курсу
+            if (!empty($lectureIds)) {
+                $sumLecturePoint = $this->lectureUserPointService->getSumLecturePoint($user->getId(), $lectureIds)[0];
+                $courseUserPointModel =  $this->modelFactory->makeModel(
+                    CourseUserPointModel::class,
+                    $user,
+                    $course,
+                    (int)$sumLecturePoint['sumLecturePoint'],
+                    1
+                );
+
+                $this->courseUserPointService->createCourseUserPoint($courseUserPointModel);
+            }
+
         }
 
         return self::MSG_ACK;
--- src/Domain/Service/LectureUserPointService.php
+++ src/Domain/Service/LectureUserPointService.php
@@ -3,14 +3,15 @@
 namespace App\Domain\Service;
 
 use App\Infrastructure\Repository\LectureUserPointRepository;
+use App\Infrastructure\Repository\CourseUserPointRepository;
 use App\Domain\Model\LectureUserPointModel;
 use App\Domain\Entity\LectureUserPoint;
-use App\Domain\Entity\User;
 
 class LectureUserPointService
 {
-    public function __construct(private readonly LectureUserPointRepository $lectureUserPointRepository)
-    {
+    public function __construct(
+        private readonly LectureUserPointRepository $lectureUserPointRepository,
+    ) {
     }
 
     public function createLectureUserPoint(LectureUserPointModel $lectureUserPointModel): LectureUserPoint
@@ -24,5 +25,11 @@ class LectureUserPointService
         $this->lectureUserPointRepository->create($lectureUserPoint);
 
         return $lectureUserPoint;
+    }
+
+
+    public function getSumLecturePoint(int $userId, array $courseId): array
+    {
+        return $this->lectureUserPointRepository->getSumLecturePoint($userId, $courseId);
     }
 }
